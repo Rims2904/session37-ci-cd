bc58e36f8056648c7fe6290b427859e432344051
Rima Chhaya <rims29@hotmail.com>
2026-08-31 22:55:50 -0400

    Added customer transformation

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@ def extract_data():
     print("Extracting data")
 
 def transform_data():
-    print("Transforming data")
+    print("Transforming customer data")
 
 def load_data():
     print("Loading data")
